367168f4aabc94e0b9ef3301adbca2f2d70a1899
Brayan Escobar <bescobarwant@gmail.com>
2025-07-22 22:19:55 -0400

    refactor(AuthController): elimina imports no utilizados

--- src/main/java/com/app/controller/AuthController.java
+++ src/main/java/com/app/controller/AuthController.java
@@ -1,26 +1,18 @@
+
 package com.app.controller;
 
-import com.app.config.jwt.JwtUtils;
 import com.app.dto.AuthRequest;
 import com.app.dto.AuthResponse;
 import com.app.dto.CreateUserDTO;
-import com.app.dto.UserResponseDTO;
-import com.app.entity.UserEntity;
 import com.app.service.AuthService;
-import com.app.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.userdetails.User;
 
 
 @RestController
