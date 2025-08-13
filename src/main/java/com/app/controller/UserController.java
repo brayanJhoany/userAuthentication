768533b06dd087b9fb5d3b63a8373ae9d9f1f2c6
Brayan Escobar <bescobarwant@gmail.com>
2025-08-12 21:21:14 -0400

    refactor: actualizar UserController para usar inyección con @Autowired

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -4,18 +4,15 @@ import com.app.dto.CreateUserDTO;
 import com.app.dto.UpdateUserDTO;
 import com.app.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity<?> getUsersPaginate(
